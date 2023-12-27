d2cbe795f05cbc411366556f90fbac3f07c735de
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-12-27 20:29:40 +0530

    deploy

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        
         Still under construction
         
       </main>
